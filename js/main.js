2a7f5a032e9cdc53a566655a76ecba359a3a4a0d
newsaeta <isaacpda@yahoo.es>
2017-09-28 19:03:59 +0200

    index terminado

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+/*var modal = document.getElementById('img-modal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('img-center');
+var modalImg = document.getElementById("img-center");
+//var captionText = document.getElementById("caption");
+
+img.addEventListener('click', () =>{
+    //alert('ministro');
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+})
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+*/
